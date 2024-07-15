75a4ee912f5df9777bc84d91f13828179f2d4cf9
2No2Name <2no2name@web.de>
2024-07-15 17:01:49 +0200

    fix: dismounting moving boat leading to wrong boat velocity

--- src/main/java/mendedboats/mixin/boat/BoatEntityMixin.java
+++ src/main/java/mendedboats/mixin/boat/BoatEntityMixin.java
@@ -2,9 +2,12 @@ package mendedboats.mixin.boat;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,10 +16,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BoatEntity.class)
 public abstract class BoatEntityMixin extends Entity {
 
+    @Shadow protected abstract void updateVelocity();
 
     public BoatEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
+
+    boolean wasPlayerControlled = false;
 
     @Inject(
             method = "updateTrackedPositionAndAngles(DDDFFI)V",
@@ -30,15 +36,31 @@ public abstract class BoatEntityMixin extends Entity {
         }
     }
 
+    @Inject(
+            method = "tick",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;updateVelocity()V")
+    )
+    private void setCartPosLikeOtherEntities(CallbackInfo ci) {
+        if (this.getWorld().isClient) {
+            this.wasPlayerControlled = true;
+        }
+    }
+
 
     @Redirect(
             method = "tick",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;isLogicalSideForUpdatingMovement()Z"))
-    private boolean simulateCartsOnClientLikeOnServer(BoatEntity instance) {
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
+    private void simulateCartsOnClientLikeOnServer(BoatEntity instance, Vec3d vec3d) {
         if (this.getWorld().isClient) {
-            return true;
+            if (this.wasPlayerControlled) {
+                this.wasPlayerControlled = false;
+                this.setVelocity(Vec3d.ZERO);
+            }
+
+            this.updateVelocity();
+            this.move(MovementType.SELF, this.getVelocity());
         } else {
-            return instance.isLogicalSideForUpdatingMovement();
+            instance.setVelocity(vec3d);
         }
     }
 }
